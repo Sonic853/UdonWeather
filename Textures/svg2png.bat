9fa35198c236e9fd88568c407ac4bab94e3c86c6
853 <sonic853@qq.com>
2026-04-16 16:36:29 +0800

    查漏补缺

--- Textures/svg2png.bat
+++ Textures/svg2png.bat
@@ -1,6 +1,6 @@
 @echo off
 if "%~1"=="" (
-    echo 使用方法: %0 input_dir output_dir width height
+    echo Usage: %0 input_dir output_dir width height
     exit /b 1
 )
 
@@ -14,12 +14,11 @@ if not exist "%output_dir%" (
 )
 
 for %%f in ("%input_dir%\*.svg") do (
-    if not exist "%output_dir%\%%~nf.png" (
-        @REM inkscape -w %width% -h %height% "%%f" -o "%output_dir%\%%~nf.png"
-        convert "%%f" -size %width%x%height% "%output_dir%\%%~nf.png"
-    ) else (
-        echo 文件已存在，跳过转换: "%output_dir%\%%~nf.png"
+    if exist "%output_dir%\%%~nf.png" (
+        del "%output_dir%\%%~nf.png"
     )
+    @REM magick "%%f" -background none -size %width%x%height% "%output_dir%\%%~nf.png"
+    inkscape -w %width% -h %height% "%%f" -o "%output_dir%\%%~nf.png"
 )
 
-echo 完成所有 SVG 文件的转换
+echo SVG to PNG conversion complete.
